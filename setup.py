70ffed0ba2f2e887e0770a2ea393cd4850891c08
riturajs19 <rituraj.singh.met23@itbhu.ac.in>
2025-03-23 23:40:27 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup = (
+    name = 'mlproject'
+    version = '0.0.1'
+    author = 'riturajs19'
+    email = 'rituraj.singh.met23@itbhu.ac.in'
+    packages = find_packages()
+    install_requires = ['pandas','numpy', 'seaborn']
+    
+)
